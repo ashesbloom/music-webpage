78df09291ec92f5bdd1d7ed9f88a549697ff6a7a
Mayank Pandey <mayankpandeydk123@gmail.com>
2023-11-26 00:01:11 +0530

    -added playlist.css and playlist.html

--- support.js
+++ support.js
@@ -1,1 +1,25 @@
 const music = new Audio("playback_tree/songs/joji/01. Glimpse of Us.mp3");
+
+
+
+
+// volumeControl.js
+
+function rangeSlider(value) {
+    document.getElementById("v_num").innerHTML = value;
+    let volumeIcon = document.getElementById('volume_icon');
+    if (value === '0') {
+        volumeIcon.className = 'fa-solid fa-volume-mute';
+    }
+    if (value < '50' && value !== '0') {
+        volumeIcon.className = 'fa-solid fa-volume-low';
+    }
+    if (value > '50' || value === '100') {
+        volumeIcon.className = 'fa-solid fa-volume-high';
+    }
+}
+
+setInterval(function () {
+    var currentValue = document.getElementById("v_bar").value;
+    rangeSlider(currentValue);
+}, 100);
