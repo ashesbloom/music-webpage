a4b6a9faa2259f6b863aab3a6828577ff20f0b72
Mayank Pandey <mayankpandeydk123@gmail.com>
2023-11-27 06:39:22 +0530

    -backend

--- support.js
+++ support.js
@@ -130,6 +130,7 @@ const songs = [
     },
 ]
 
+
 document.addEventListener('DOMContentLoaded', function() {
     const songItems = document.querySelectorAll('.songs_list .song_items');
 
@@ -142,6 +143,7 @@ document.addEventListener('DOMContentLoaded', function() {
         da.innerHTML = songs[index].time;
     });
 });
+
 
 document.addEventListener('DOMContentLoaded', function () {
     let masterplay = document.getElementById('master_play');
@@ -352,3 +354,78 @@ setInterval(function () {
     let currentValue = document.getElementById("v_bar").value;
     rangeSlider(currentValue);
 }, 100);
+document.addEventListener('DOMContentLoaded', function () {
+    let availableKeywords = [
+        'ew',
+        'modus',
+        'tick tock',
+        'daylight',
+        'gimme love',
+        'run',
+
+    ];
+
+    const resultbox = document.querySelector(".resultbox");
+    const inputbox = document.getElementById("inputbox");
+
+    inputbox.addEventListener("input", function () {
+        let input = inputbox.value.toLowerCase();
+        let result = availableKeywords.filter(keyword => keyword.toLowerCase().includes(input));
+        display(result);
+    });
+
+    const songImageMapping = {
+        'Ew': 'playback_tree/covers/joji/joji_nector.jpg',
+        'Modus': 'playback_tree/covers/joji/joji_nector.jpg',
+        'Tick Tock': 'playback_tree/covers/joji/joji_nector.jpg',
+        'Daylight': 'playback_tree/covers/joji/joji_daylight.jpg',
+        'Gimme Love': 'playback_tree/covers/joji/joji_golden.jpg',
+        'Run': 'playback_tree/covers/joji/joji_run.jpg',
+
+        // 'Ew': 'playback_tree/covers/joji/joji_nector.jpg',
+        // Add more entries as needed
+    };
+
+    function display(result) {
+        const content = result.map((list) => {
+            const imageName = list.replace(/\s+/g, '_').toLowerCase();
+            const imagePath = songImageMapping[imageName];
+            const fileName = list.replace(/\s+/g, '_').toLowerCase() + '.html';
+
+            return `<li><img src="${imagePath}" alt="${list}" style="width: 30px; height:30px; margin-right:10px;"> <a href="${fileName}">${list}</a> </li>`;
+        });
+
+        // Assuming resultbox is a ul or ol element
+        resultbox.innerHTML = content.join('');
+        resultbox.innerHTML = `<ul>${content.join('')}</ul>`;
+        resultbox.style.display = result.length > 0 ? 'block' : 'none';
+    }
+    function calculateImageSize(songName) {
+        // Adjust the base size and factor based on your preference
+        const baseSize = 30;
+        const sizeFactor = 2;
+        return baseSize + sizeFactor * songName.length;
+    }
+
+    document.addEventListener("click", function (event) {
+        if (!resultbox.contains(event.target) && event.target !== inputbox) {
+            resultbox.style.display = 'none';
+        }
+    });
+
+    resultbox.addEventListener("click", function (event) {
+        if (event.target.tagName === 'LI') {
+            inputbox.value = event.target.textContent;
+            resultbox.style.display = 'none';
+        }
+    });
+});
+function selectInput(element) {
+    const imageName = element.id;
+    console.log("selectInput function called");
+
+    const pagePath = `${imageName}.html`;
+
+    // Open the web page in the same tab
+    window.open(pagePath, '_blank');
+}
